5d7594ba84a87db181fca75715f7c34910995f61
Maciek <mdyrdal@student.agh.edu.pl>
2023-12-10 15:04:52 +0100

    Add logging library

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 //Using SDL and standard IO
+#include <plog/Log.h>
+#include <plog/Initializers/RollingFileInitializer.h>
 #include <SDL.h>
 #include <iostream>
 
@@ -8,6 +10,10 @@ constexpr int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* args[])
 {
+	//Initialize the logger
+	plog::init(plog::debug, "log.txt");
+	PLOG_INFO << "Starting app...";
+
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
 
@@ -17,7 +23,8 @@ int main(int argc, char* args[])
 	//Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 	{
-		std::cout << "SDL could not initialize! SDL_Error: \n" << SDL_GetError();
+		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << '\n';
+		PLOG_DEBUG << "SDL could not initialize! SDL_Error: " << SDL_GetError() << '\n';
 	}
 	else
 	{
@@ -25,7 +32,8 @@ int main(int argc, char* args[])
 		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 		{
-			std::cout << "Window could not be created! SDL_Error: \n" << SDL_GetError();
+			std::cout << "Window could not be created! SDL_Error: " << SDL_GetError() << '\n';
+			PLOG_DEBUG << "Window could not be created! SDL_Error: " << SDL_GetError() << '\n';
 		}
 		else
 		{
@@ -48,6 +56,8 @@ int main(int argc, char* args[])
 
 	//Quit SDL subsystems
 	SDL_Quit();
+
+	PLOG_INFO << "Exiting app...\n";
 
 	return 0;
 }
